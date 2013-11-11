8628a60c7af3999da2e90e7e6c0335144fb58ec5
Brook <uedsky@gmail.com>
2013-11-11 11:10:47 +0800

    fix symlink cwd

--- scripts/get_cwd.py
+++ scripts/get_cwd.py
@@ -2,13 +2,12 @@
 # -*- coding: utf-8 -*-
 
 # http://jagt.github.io/python-single-line-convert/
-
 import os,re
-home, names = os.getenv("HOME"), os.getcwd().split(os.sep)
+home, names = os.getenv("HOME"), os.getenv('PWD').split(os.sep)
 if not names[0]: names.pop(0)
 path = ""
-for i in range(len(names)):
-    path += os.sep + names[i]
+for i,name in enumerate(names):
+    path += os.sep + name
     if os.path.samefile(path, home):
         names = ["~"] + names[i+1:]
         break
@@ -16,4 +15,9 @@ if names[0] != "~": names[0] = "/" + names[0]
 max_depth = 5
 if len(names) > max_depth:
     names = names[:2] + ["..."] + names[3 - max_depth:]
-print os.sep.join(names)
+print os.sep.join(names),
+
+try:
+    os.getcwd()
+except:
+    print '(Invalid CWD)'
